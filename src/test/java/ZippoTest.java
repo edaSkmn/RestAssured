47baddf5ba17f202fdd864923e9953f8d1494d3a
edaSkmn <edasekmen@windowslive.com>
2023-01-08 17:38:15 +0300

    RestAssured1

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,25 +1,26 @@
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class ZippoTest {
 
     @Test
-    public void test(){
+    public void test() {
 
         given()
                 // hazırlık işlemlerini yapacağız (token,send body, parametreler)
                 .when()
                 // link i ve metodu veriyoruz
                 .then()
-                 //  assertion ve verileri ele alma extract
+        //  assertion ve verileri ele alma extract
         ;
 
     }
 
     @Test
-    public void statusCodeTest(){
+    public void statusCodeTest() {
 
         given()
 
@@ -32,8 +33,9 @@ public class ZippoTest {
         ;
 
     }
+
     @Test
-    public void contentTypeTest(){
+    public void contentTypeTest() {
 
         given()
 
@@ -47,8 +49,9 @@ public class ZippoTest {
         ;
 
     }
+
     @Test
-    public void checkCountryInResponseTest(){
+    public void checkCountryInResponseTest() {
 
         given()
 
@@ -58,7 +61,7 @@ public class ZippoTest {
                 .then()
                 .log().body() // log().all() butun response u / bodyi gosterir
                 .statusCode(200) // status kontrolu
-                .body("country",equalTo("United States")) // body.country= United States ?
+                .body("country", equalTo("United States")) // body.country= United States ?
         ;
 
     }
@@ -70,7 +73,7 @@ public class ZippoTest {
     //body.places.'place name'        body("places.'place name'")   -> bütün place name leri verir
     //                                bir index verilmezse dizinin bütün elemanlarında arar
     @Test
-    public void checkStateInResponseTest(){
+    public void checkStateInResponseTest() {
 
         given()
 
@@ -80,12 +83,13 @@ public class ZippoTest {
                 .then()
                 .log().body() // log().all() butun response u / bodyi gosterir
                 .statusCode(200) // status kontrolu
-                .body("places[0].state",equalTo("California")) // body.country= United States ?
+                .body("places[0].state", equalTo("California")) // body.country= United States ?
         ;
 
     }
+
     @Test
-    public void checkStateInResponseTest3(){
+    public void checkStateInResponseTest3() {
 
         given()
 
@@ -93,14 +97,15 @@ public class ZippoTest {
                 .get("http://api.zippopotam.us/tr/01000")
 
                 .then()
-               // .log().body() // log().all() butun response u / bodyi gosterir
+                // .log().body() // log().all() butun response u / bodyi gosterir
                 .statusCode(200) // status kontrolu
                 .body("places.'place name'", hasItem("Büyükdikili Köyü")) // verilen path  deki liste bu item a sahip mi/iceriyor mu
         ;
 
     }
+
     @Test
-    public void bodyArrayHasSizeTest(){
+    public void bodyArrayHasSizeTest() {
 
         given()
 
@@ -110,12 +115,13 @@ public class ZippoTest {
                 .then()
                 .log().body() // log().all() butun response u / bodyi gosterir
                 .statusCode(200) // status kontrolu
-                .body("places",hasSize(1)) // place in size i 1 e esit mi
+                .body("places", hasSize(1)) // place in size i 1 e esit mi
         ;
 
     }
+
     @Test
-    public void combiningTest(){
+    public void combiningTest() {
 
         given()
 
@@ -125,11 +131,90 @@ public class ZippoTest {
                 .then()
                 .log().body() // log().all() butun response u / bodyi gosterir
                 .statusCode(200) // status kontrolu
-                .body("places",hasSize(1)) // place in size i 1 e esit mi
+                .body("places", hasSize(1)) // place in size i 1 e esit mi
                 .body("places.state", hasItem("California")) // verilen path  deki liste bu item a sahip mi/iceriyor mu
                 .body("places[0].'place name'", equalTo("Beverly Hills")) // verilen pathdeki deger buna esit mi
         ;
 
     }
 
+    @Test
+    public void pathParamTest() {
+
+        given()
+                .pathParam("Country", "us")
+                .pathParam("Zipkod", 90210)
+                .log().uri() // request link Request URI:	http://api.zippopotam.us/us/90210
+
+                .when()
+                .get("http://api.zippopotam.us/{Country}/{Zipkod}")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+
+    }
+
+    @Test
+    public void pathParamTest2() {
+        // 90210 dan 90213 kadar test sonuçlarında places in size nın hepsinde 1 gediğini test ediniz.
+        for (int i = 90210; i <= 90213; i++) {
+
+
+            given()
+                    .pathParam("Country", "us")
+                    .pathParam("Zipkod", 90210)
+                    .log().uri() // request link Request URI:	http://api.zippopotam.us/us/90210
+
+                    .when()
+                    .get("http://api.zippopotam.us/{Country}/{Zipkod}")
+
+                    .then()
+                    .log().body()
+                    .statusCode(200)
+                    .body("places", hasSize(1))
+            ;
+        }
+    }
+    @Test
+    public void queryParamTest() {
+        //https://gorest.co.in/public/v1/users?page=3
+
+        given()
+                .param("page",1) // ?page=1  seklinde ekleniyor
+                .log().uri()
+
+                .when()
+                .get("https://gorest.co.in/public/v1/users")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("meta.pagination.page", equalTo(1))
+        ;
+
+    }
+    @Test
+    public void queryParamTest2() {
+        //https://gorest.co.in/public/v1/users?page=3
+        // bu linkteki 1 den 10 kadar sayfaları çağırdığınızda response daki donen page degerlerinin
+        // çağrılan page nosu ile aynı olup olmadığını kontrol ediniz.
+
+        for (int pageNo = 1; pageNo <=10 ; pageNo++) {
+
+        given()
+                .param("page",pageNo) // ?page=1  seklinde ekleniyor
+                .log().uri()
+
+                .when()
+                .get("https://gorest.co.in/public/v1/users")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("meta.pagination.page", equalTo(pageNo))
+        ;
+        }
+    }
 }
